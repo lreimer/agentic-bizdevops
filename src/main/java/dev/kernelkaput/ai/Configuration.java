62ec89fb13c279b22bf58604fb14c2df4da0a823
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2025-06-03 10:03:11 +0200

    Added some code that will trigger Sonar violations.

---
+++ src/main/java/dev/kernelkaput/ai/Configuration.java
@@ -0,0 +1,12 @@
+package dev.kernelkaput.ai;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Configuration {
+
+    public static final String OPENAI_API_KEY = "sk-proj-1234567890abcdefg";
+
+    public static final String MASTER_PASSWORD = "change_me";
+
+}
--- src/main/java/dev/kernelkaput/ai/GreetingResource.java
+++ src/main/java/dev/kernelkaput/ai/GreetingResource.java
@@ -1,5 +1,6 @@
 package dev.kernelkaput.ai;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
